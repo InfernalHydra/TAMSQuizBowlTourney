b54465112f8a0ca89c7908ff402df986532a68e0
Ryan Chhong <ryanchhong@gmail.com>
2018-07-18 00:13:14 -0500

    Added async stuff

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
+const async = require('async');
+const parallel = require('async/parallel');
 const express = require('express');
 const app = express();
 const path = require('path')
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_standings.html'));
-});
+async.parallel([
+  () => {app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_standings.html'));
+  })},
+  () => {app.get('/standings', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_standings.html'));
+  })},
+  () => {app.get('/games', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_games.html'));
+  })},
+  () => {app.get('/teamdetail', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_teamdetail.html'));
+  })},
+  () => {app.get('/playerdetail', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_playerdetail.html'));
+  })},
+  () => {app.get('/rounds', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_rounds.html'));
+  })},
+  () => {app.get('/statkey', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_statkey.html'));
+  })},
+  () => {app.get('/individuals', (req, res) => {
+      res.sendFile(path.join(__dirname + '/static/Report_individuals.html'));
+  })},
 
-app.get('/standings', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_standings.html'));
-});
-
-app.get('/games', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_games.html'));
-});
-
-app.get('/teamdetail', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_teamdetail.html'));
-});
-
-app.get('/playerdetail', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_playerdetail.html'));
-});
-
-app.get('/rounds', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_rounds.html'));
-});
-
-app.get('/statkey', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_statkey.html'));
-});
-
-app.get('/individuals', (req, res) => {
-    res.sendFile(path.join(__dirname + '/static/Report_individuals.html'));
-});
+]);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
