95089ac7446b05fc3547d944e64a0b7a3d0d5378
TheBunanna <thebunanna@gmail.com>
2018-07-18 23:04:51 -0500

    compression

--- js/script1.js
+++ js/script1.js
@@ -2,8 +2,8 @@
 $(document).ready(()=> {
   const links = new Array(7);
   var heading = $("table").first();
-  var tables = heading.children().children().children();
-  var currentChild = tables
+  var blinks = heading.children().children().children();
+  var currentChild = blinks
   for (let i = 0; i < 7; i++) {
     links[i] = currentChild.children();
     currentChild = currentChild.next()
@@ -39,79 +39,104 @@ $(document).ready(()=> {
 
   $("h1").first().attr("class", "title");
   //console.log($('tr').first().html());
-  currentChild = $('tr').first().children();
-  for (let i = 0; i < 20; i++) {
-    var node = null
-    var toolt = null
-    var no = false
-    currentChild.attr("class", "tooltip");
-    console.log (currentChild.html());
-    //console.log (currentChild.find("b").html());
-    switch (currentChild.find("b").html()) {
-      case "W":
-        toolt = "wins";
-        break;
-      case "L":
-        toolt = "losses";
-        break;
-      case "T":
-        toolt = "ties";
-        break;
-      case "Pct":
-        toolt = "Fraction of games won";
-        break;
-      case "PPG":
-        toolt = "Average number of points";
-        break;
-      case "PAPG":
-        toolt =	"Average number of points against";
-        break;
-      case "Mrg":
-        toolt = "Margin of victory";
-        break;
-      case "15":
-        toolt = "Power";
-        break;
-      case "10":
-        toolt = "Toss-Up";
-        break;
-      case "-5":
-        toolt = "Neg"
-        break;
-      case "TUH":
-        toolt = "Total number of toss-ups heard";
-        break;
-      case "PPTH":
-        toolt = "avg score per toss";
-        break;
-      case "P/N":
-        toolt = "power to neg ratio";
-        break;
-      case "G/N":
-        toolt = "get to neg ratio";
-        break;
-      case "BHrd":
-        toolt = "Total bonus";
-        break;
-      case "BPts":
-        toolt = "Points for bonus";
-        break;
-      case "P/B":
-        toolt = "avg score for bonus";
-        break;
-      default:
-        no = true;
+  const obj = {"W":"wins","L":"losses","T":"ties",
+  "Pct":"Fraction of games won","PPG":"Average number of points",
+  "PAPG":"Average number of points against","Mrg":"Margin of victory",
+  "15":"Power","10": "Toss-Up","-5":"Neg","TUH":"Total number of toss-ups heard",
+  "PPTH":"avg score per toss","P/N":"power to neg ratio","G/N":"get to neg ratio",
+  "BHrd":"Total bonus","BPts":"Points for bonus","P/B":"avg score for bonus"}
+  $('table').each(function () {
+    $(this).find('tr').first().children().each(function() {
+      $(this).attr("class","tooltip");
+      let text = $(this).find("b").html()
+      if (text in obj) {
+        node = $('<span class="tooltiptext">' + obj[text] + '</span>');
+        $(this).find("b").first().after(node)
       }
-      node = $('<span class="tooltiptext">' + toolt + '</span>');
-      console.log(node.html());
-      if (no) {
+      else {
+        console.log("NOPE");
+      }
+    })
+  })
+  /*
+  var tables = $('table');
+
+    currentChild = tables.first().find('tr').first().children();
+
+    const size = currentChild.length;
+    console.log (size);
+    for (let i = 0; i < size; i++) {
+      var node = null
+      var toolt = null
+      var no = false
+      currentChild.attr("class", "tooltip");
+      switch (currentChild.find("b").html()) {
+        case "W":
+          toolt = "wins";
+          break;
+        case "L":
+          toolt = "losses";
+          break;
+        case "T":
+          toolt = "ties";
+          break;
+        case "Pct":
+          toolt = "Fraction of games won";
+          break;
+        case "PPG":
+          toolt = "Average number of points";
+          break;
+        case "PAPG":
+          toolt =	"Average number of points against";
+          break;
+        case "Mrg":
+          toolt = "Margin of victory";
+          break;
+        case "15":
+          toolt = "Power";
+          break;
+        case "10":
+          toolt = "Toss-Up";
+          break;
+        case "-5":
+          toolt = "Neg"
+          break;
+        case "TUH":
+          toolt = "Total number of toss-ups heard";
+          break;
+        case "PPTH":
+          toolt = "avg score per toss";
+          break;
+        case "P/N":
+          toolt = "power to neg ratio";
+          break;
+        case "G/N":
+          toolt = "get to neg ratio";
+          break;
+        case "BHrd":
+          toolt = "Total bonus";
+          break;
+        case "BPts":
+          toolt = "Points for bonus";
+          break;
+        case "P/B":
+          toolt = "avg score for bonus";
+          break;
+        default:
+          no = true;
+        }
+        node = $('<span class="tooltiptext">' + toolt + '</span>');
+        if (no) {
+          currentChild = currentChild.next();
+          console.log ("NOPE");
+          continue;
+        }
+        currentChild.find("b").first().after(node);
         currentChild = currentChild.next();
-        console.log ("NOPE");
-        continue;
-      }
-      currentChild.find("b").first().after(node);
-      currentChild = currentChild.next();
-    }
+      }*/
+
+
+
 
 
 })
