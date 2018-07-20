254b96d9b367b8ee9a42e34aae7c459d3cadce85
TheBunanna <thebunanna@gmail.com>
2018-07-19 19:46:10 -0500

    some js changes

--- js/header.js
+++ js/header.js
@@ -40,21 +40,22 @@ $(document).ready(()=> {
 
   $("h1").first().attr("class", "title");
   //console.log($('tr').first().html());
-  const obj = {"W":"wins","L":"losses","T":"ties",
+  const obj = {"W":"Wins","L":"Losses","T":"Ties",
   "Pct":"Fraction of games won","PPG":"Average number of points",
   "PAPG":"Average number of points against","Mrg":"Margin of victory",
   "15":"Power","10": "Toss-Up","-5":"Neg","TUH":"Total number of toss-ups heard",
-  "PPTH":"avg score per toss","P/N":"power to neg ratio","G/N":"get to neg ratio",
-  "BHrd":"Total bonus","BPts":"Points for bonus","P/B":"avg score for bonus"}
+  "PPTH":"Avg score per toss","P/N":"Power to neg ratio","G/N":"Get to neg ratio",
+  "BHrd":"Total bonus","BPts":"Points for bonus","P/B":"Avg score for bonus",
+  "GP":"Games Played", "P/TU":"Avg per toss-up","Pts":"Points",
+  "PF":"Poinsts Scored (for)","PA":"Points scored (against)"}
   var sorting = []
   var sValues = []
   var tags = []
-  var pos = 0
   var current = -1
   $('table').each(function (index) {
     $(this).attr("class", "highlight");
     sorting.push([]);
-    pos = index;
+    var pos = index;
     $(this).find('tr').attr("class","tbh");
     $(this).find('tr').first().children().each(function(index) {
       //console.log(index);
@@ -67,38 +68,52 @@ $(document).ready(()=> {
       }
 
       $(this).click(() => {
-        if(url.indexOf('http://' + host + '/teamdetail') != -1) {
+
+        if(url.indexOf('http://' + host + '/statkey') != -1) {
           return;
-        }
+        }/*
         if(url.indexOf('http://' + host + '/playerdetail') != -1) {
           return;
-        }
+        }*/
         current = current == -1 ? 1 : -1;
         var comparison = {};
-        for (let i = 0; i < sValues.length;i++) {
+        var hasb = 0
+        if (sorting[pos][sorting[pos].length-1].has('b').length > 0) {
+          hasb = 1
+        }
+
+        for (let i = 0; i < sValues.length - hasb;i++) {
+          if (sValues[i][index] == "inf") {
+            comparison[i] = 1000000;
+          }
           comparison[i]=sValues[i][index]
         }
         var sorted = Object.keys(comparison).sort(function(a,b){
+
           if (comparison[a] == comparison[b]) {
             return a < b ? 1 : -1;
           }
           return comparison[a]-comparison[b];
         })
-
-        $(this).closest("table").find('tr').first().nextAll().each(function () {
+        var ob = $(this).closest("table").find('tr').first()
+          ob.nextAll().each(function () {
           $(this).remove();
         })
 
         if (current == 1) {
-          for (i = 0; i < sValues.length; i++) {
-            $(this).closest("table").find('tr').first().after(sorting[pos][sorted[i]]);
+          for (i = 0; i < sValues.length-hasb; i++) {
+            ob.after(sorting[pos][sorted[i]]);
           }
         }
         else {
-          for (i = sValues.length - 1; i >= 0; i--) {
-          $(this).closest("table").find('tr').first().after(sorting[pos][sorted[i]]);
+          for (i = sValues.length - 1 - hasb; i >= 0; i--) {
+          ob.after(sorting[pos][sorted[i]]);
           }
         }
+        if (hasb == 1) {
+          var ob = $(this).closest("table").find('tr').last().after(sorting[pos][sorting[pos].length-1]);
+        }
+
       })
     })
 
@@ -140,15 +155,16 @@ $(document).ready(()=> {
     })
   })
   var endq=null
+  /*
   if(url.indexOf('http://' + host + '/teamdetail') != -1) {
     endq = $("<div class='title'>The tags here a bit buggy so sorting has been disabled</div>");;
   }
   if(url.indexOf('http://' + host + '/playerdetail') != -1) {
     endq = $("<div class='title'>The tags here a bit buggy so sorting has been disabled</div>");;
-  }
-  else {
-    endq = $("<div class='title'>Click on the tags to sort</div>");
-  }
+  }*/
+
+  endq = $("<div class='title'>Click on the tags to sort</div>");
+
   var endq2 = $("<div class='title'>Made by Daniel Hahn, Aditya Paul, and Ryan Chhong</div>");
   var endpic = $("<img class='ending'src='background.jpg'></img>");
   $("body").children().last().after(endq);
